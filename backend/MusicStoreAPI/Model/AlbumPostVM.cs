72b101437d6fb2473939ca68498fb838411b6992
Shreyas Sakhalkar <116295725+sshreyas11@users.noreply.github.com>
2024-07-24 16:48:14 -0400

    Create AlbumPostVM.cs
    Post API VM

---
+++ backend/MusicStoreAPI/Model/AlbumPostVM.cs
@@ -0,0 +1,14 @@
+namespace MusicStoreWebApp.Models
+{
+    public class AlvumPostVM
+    {
+       
+        public string album_name { get; set; }
+        public double album_price { get; set; }
+        public string album_release_date { get; set; }
+        public string album_artist { get; set; }
+        public string album_genre { get; set; }
+
+
+    }
+}

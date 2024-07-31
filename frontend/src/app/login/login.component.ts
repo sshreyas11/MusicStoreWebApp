e770394b2cae6553675a8acd4baa34de69d07158
Shreyas Sakhalkar <116295725+sshreyas11@users.noreply.github.com>
2024-07-31 15:30:38 -0400

    Update login.component.ts

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -11,33 +11,40 @@ import { Router } from '@angular/router';
 })
 
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
   error_message: string = '';
-  login: Login = new Login();
+//  login: Login = new Login();
 
   constructor(public login_service:LoginService, private router: Router){}
-
-  onSubmit():void{
-    this.login_service.login(
-      this.login.email,
-      this.login.password
-    ).subscribe(
-      success=>{
-        if(success){
-          console.log("Login Successful");
+  login(){
+    this.login_service.login(this.username, this.password).subscribe(
+      respons =>{
+        if(respons){
           this.router.navigate(['/albums']);
         }else{
           this.error_message = 'Invalid Login';
         }
+
+      },
+      error =>{
+        this.error_message ='An error has occurred';
       }
     );
   }
 }
-
-class Login{
-  email: string;
-  password:string;
-  constructor(){
-    this.email = '';
-    this.password = '';
-  }
-}
+  // onSubmit():void{
+  //   this.login_service.login(
+  //     this.login.email,
+  //     this.login.password
+  //   ).subscribe(
+  //     success=>{
+  //       if(success){
+  //         console.log("Login Successful");
+  //         this.router.navigate(['/albums']);
+  //       }else{
+  //         this.error_message = 'Invalid Login';
+  //       }
+  //     }
+  //   );
+  // }

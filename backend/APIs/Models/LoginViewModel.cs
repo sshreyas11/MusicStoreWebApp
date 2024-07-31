58754199e0a492c8bfe1fe684e30e9b9634b24e1
Shreyas Sakhalkar <116295725+sshreyas11@users.noreply.github.com>
2024-07-31 15:43:28 -0400

    Create LoginViewModel.cs

---
+++ backend/APIs/Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace APIs.Models {
+  public class LoginViewModel
+    {
+        [Required]
+        public string cust_username { get; set; }
+
+        [Required]
+        public string cust_password { get; set; }
+    }
+}

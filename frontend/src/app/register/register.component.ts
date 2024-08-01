e2d6b5cd8de20f4892870d2f45e2eef68da409ec
Anirudh Dutt <anirudh.dutt1@incedoinc.com>
2024-08-01 00:43:27 -0400

    Added session storage

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user/user.service';
 import { User } from '../models/user.model';
 import { Router } from '@angular/router';
-import * as CryptoJS from 'crypto-js';
-
+import { LoginService } from '../services/login/login.service';
 
 @Component({
   selector: 'app-register',
@@ -15,13 +14,11 @@ export class RegisterComponent {
   user_types: string[] = ['Employee', 'Customer'];
   error_message: string = '';
  
-  constructor(private userService:UserService, private router:Router){}
+  constructor(private userService:UserService, private router:Router, public loginService:LoginService){}
  
 
   onSubmit(){
     console.log(this.user);
-    // const hashedPassword = CryptoJS.SHA256(this.user.password_hash).toString();
-    // this.user.password_hash = hashedPassword;
     this.userService.register(this.user).subscribe(
       response => {
         console.log('Registration successful', response);
@@ -32,5 +29,9 @@ export class RegisterComponent {
         this.error_message = 'Registration failed. Please try again.';
       }
     );
+  }
+  
+  get_user(){
+    return this.loginService.usr_name;
   }
 }
--- frontend/src/app/services/login/login.service.ts
+++ frontend/src/app/services/login/login.service.ts
@@ -1,16 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+export const AUTH_TOKEN_KEY = 'auth-token';
+export const usr_data = 'usr-name';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
+  public authToken: string | null = null;
+  public usr_name: string | null = null;
   private apiURL = 'http://localhost:5283/api/User/Login';
   constructor(private http: HttpClient) {}
 
-   login(username:string, password:string): Observable<any>{
-    return this.http.post<any>(this.apiURL, {cust_email:username, cust_password:password});
-   }
+  checkStorage() {
+    const authToken = sessionStorage.getItem(AUTH_TOKEN_KEY);
+    const firstName = sessionStorage.getItem(usr_data);
+    this.authToken = authToken;
+    if (firstName) {
+      this.usr_name = firstName;
+    } else {
+      this.usr_name = null;
+    }
+  }
+
+  login(email: string, password: string): Observable<any> {
+    sessionStorage.setItem(AUTH_TOKEN_KEY, email);
+    this.checkStorage();
+    return this.http
+      .post<any>(this.apiURL, { cust_email: email, cust_password: password })
+      .pipe(
+        tap((response) => {
+          const firstName = response.firstName;
+          if (firstName) {
+            sessionStorage.setItem(usr_data, firstName);
+          }
+        })
+      );
+  }
+
+  public isLoggedIn(){
+    return this.authToken!==null;
+  }
+
+  public logout(){
+    if(!this.isLoggedIn()) return;
+    sessionStorage.clear();
+    this.checkStorage();
+  }
 }
